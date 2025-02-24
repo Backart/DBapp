9470133f228c9ab67632f26f0b6747e5635f16ac
Backart <artem.maksymenko2000@gmail.com>
2025-02-24 15:28:17 +0200

    test 3

---
+++ build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/SqlLiteBDApp_autogen/include/ui_adminwindow.h
@@ -0,0 +1,68 @@
+/********************************************************************************
+** Form generated from reading UI file 'adminwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.8.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_ADMINWINDOW_H
+#define UI_ADMINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableView>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_AdminWindow
+{
+public:
+    QWidget *centralwidget;
+    QTableView *tableView;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *AdminWindow)
+    {
+        if (AdminWindow->objectName().isEmpty())
+            AdminWindow->setObjectName("AdminWindow");
+        AdminWindow->resize(583, 399);
+        centralwidget = new QWidget(AdminWindow);
+        centralwidget->setObjectName("centralwidget");
+        tableView = new QTableView(centralwidget);
+        tableView->setObjectName("tableView");
+        tableView->setGeometry(QRect(10, 40, 561, 281));
+        AdminWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(AdminWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 583, 20));
+        AdminWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(AdminWindow);
+        statusbar->setObjectName("statusbar");
+        AdminWindow->setStatusBar(statusbar);
+
+        retranslateUi(AdminWindow);
+
+        QMetaObject::connectSlotsByName(AdminWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *AdminWindow)
+    {
+        AdminWindow->setWindowTitle(QCoreApplication::translate("AdminWindow", "MainWindow", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class AdminWindow: public Ui_AdminWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_ADMINWINDOW_H
