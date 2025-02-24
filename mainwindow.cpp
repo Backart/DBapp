d71fb1206491c5681f2bb7b093840f83c836ddd1
Backart <artem.maksymenko2000@gmail.com>
2025-02-24 15:14:36 +0200

    test 2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -39,7 +39,7 @@ void MainWindow::onLoginClicked()
 
     UserAuthorization auth;
     if (auth.authenticateUser(username, password, role_id, errorMessage))
-        this->close();  // close головне вікно після успішного входу fdvdfvdv
+        this->close();  // close головне вікно після успішного входу
 
 }
 
