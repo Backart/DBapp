4b879246d839cf449039a1f54d91a4cc6727fea3
Backart <artem.maksymenko2000@gmail.com>
2025-03-16 19:14:49 +0200

    fix connect to DB

--- dbconnectionmanager.cpp
+++ dbconnectionmanager.cpp
@@ -1,41 +1,4 @@
 #include "dbconnectionmanager.h"
-#include <QPluginLoader>
-
-#include <QLibraryInfo>
-#include <QProcessEnvironment>
-#include <QPluginLoader>
-
-void printQtEnvVariables() {
-    QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
-    QStringList keys = env.keys();
-
-    qDebug() << "==================== Qt Environment Variables ====================";
-    for (const QString &key : keys) {
-        if (key.startsWith("QT") || key.startsWith("LD") || key.contains("PATH", Qt::CaseInsensitive)) {
-            qDebug() << key << "=" << env.value(key);
-        }
-    }
-}
-
-void checkPostgreSQLDriverDependencies() {
-    QString driverPath = "/usr/lib/qt6/plugins/sqldrivers/libqsqlpsql.so";
-    qDebug() << "Checking dependencies for:" << driverPath;
-
-    QProcess process;
-    process.start("ldd", QStringList() << driverPath);
-    process.waitForFinished();
-    QString output = process.readAllStandardOutput();
-    QString errorOutput = process.readAllStandardError();
-
-    qDebug() << "=== ldd Output ===";
-    qDebug().noquote() << output;
-
-    if (!errorOutput.isEmpty()) {
-        qDebug() << "=== ldd Errors ===";
-        qDebug().noquote() << errorOutput;
-    }
-}
-
 
 DBConnectionManager::DBConnectionManager() {}
 
@@ -59,7 +22,6 @@ bool DBConnectionManager::openDatabase() {
         db.close();
     }
 
-
     // Видалити існуюче підключення (важливо!)
     QString connectionName = db.connectionName();
     db = QSqlDatabase(); // Очистити поточний об'єкт
@@ -69,31 +31,6 @@ bool DBConnectionManager::openDatabase() {
     QString configPath = QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation);
     configPath = QDir::cleanPath(configPath + "/dataConnectConfig.ini");
     QSettings settings(configPath, QSettings::IniFormat);
-
-
-
-    qputenv("QT_DEBUG_PLUGINS", QByteArray("1"));
-    printQtEnvVariables();
-    checkPostgreSQLDriverDependencies();
-
-    qDebug() << "Available SQL Drivers: " << QSqlDatabase::drivers();
-
-    QDir pluginsDir("/usr/lib/qt6/plugins/sqldrivers");
-    QStringList files = pluginsDir.entryList(QDir::Files);
-
-    qDebug() << "SQL driver plugins found in /usr/lib/qt6/plugins/sqldrivers:";
-    for (const QString &file : files) {
-        qDebug() << file;
-
-        QPluginLoader loader(pluginsDir.absoluteFilePath(file));
-        QObject *plugin = loader.instance();
-        if (plugin) {
-            qDebug() << "Loaded plugin: " << file;
-        } else {
-            qDebug() << "Failed to load plugin: " << file << "Error:" << loader.errorString();
-        }
-    }
-
 
     QString host = settings.value("Database/host").toString();
     int port = settings.value("Database/port").toInt();
